1f85ec625be157f46f696fe63f2424d311ac1db0
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-08-04 22:09:50 -0500

    chore: :white_check_mark: Add log and doctest calls

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,35 @@
 // Copyright (c) 2020-2022 Cesanta Software Limited
 // All rights reserved
 
+#include <cstddef>
+#include <cstdint>
+
 #include <FreeRTOS.h>
 #include <hardware/watchdog.h>
 #include <lwip/ip4_addr.h>
 #include <pico/cyw43_arch.h>
 #include <pico/stdlib.h>
 #include <portability.h>
+#include <test.hpp>
 
+#include "flash/spi.hpp"
 #include "gpib.hpp"
+#include "log.hpp"
 #include "mongoose.h"
 #include "net.h"
-// #include "spi_flash.hpp"
+#include "spi.hpp"
 #include "task.h"
 //
 #include "ff_headers.h"
 #include "ff_sddisk.h"
 #include "ff_stdio.h"
 #include "ff_utils.h"
-//
 #include "hw_config.h"
 
 #define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 #define TEST_TASK_STACK_SIZE ((configSTACK_DEPTH_TYPE)2048)
+
+using namespace Log;
 
 static inline void stop() {
     fflush(stdout);
@@ -34,7 +41,7 @@ static inline void stop() {
 static void SimpleTask(void *arg) {
     (void)arg;
 
-    printf("\n%s: Hello, world!\n", pcTaskGetName(NULL));
+    logme(Level::NOTICE, pcTaskGetName(NULL), "Hello, world!");
 
     FF_Disk_t *pxDisk = FF_SDDiskInit("sd0");
     configASSERT(pxDisk);
@@ -67,37 +74,107 @@ static void SimpleTask(void *arg) {
     vTaskDelete(NULL);
 }
 
-static struct mg_mgr mgr;
+// static struct mg_mgr mgr;
 
 constexpr uint8_t ipv4_digit(uint32_t &ip, int pos) {
     return (ip >> (8 * pos)) & 0xFF;
 }
 
 void main_task(__unused void *params) {
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return;
-    }
-    cyw43_arch_enable_sta_mode();
-    printf("Connecting to WiFi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        exit(1);
-    }
 
-    mg_mgr_init(&mgr);
-    web_init(&mgr);
-
-    {
-        uint32_t ip = *((uint32_t *)mgr.conns->loc.ip);
-        printf("Connected: %u.%u.%u.%u\n", ipv4_digit(ip, 0), ipv4_digit(ip, 1), ipv4_digit(ip, 2), ipv4_digit(ip, 3));
+    while (!stdio_usb_connected()) {
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
+    vTaskDelay(pdMS_TO_TICKS(250));
+
+    puts("Hello");
+
+    int32_t test_results = Test::baseline_test();
+
+    logme(Level::INFO, pcTaskGetName(NULL), "Test Results: %i", test_results);
+
+    //     if (cyw43_arch_init()) {
+    //         printf("failed to initialise\n");
+    //         return;
+    //     }
+    //     cyw43_arch_enable_sta_mode();
+    //     printf("Connecting to WiFi...\n");
+    //     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    //         printf("failed to connect.\n");
+    //         exit(1);
+    //     }
+
+    //     mg_mgr_init(&mgr);
+    //     web_init(&mgr);
+
+    //     {
+    //         uint32_t ip = *((uint32_t *)mgr.conns->loc.ip);
+    //         printf("Connected: %u.%u.%u.%u\n", ipv4_digit(ip, 0), ipv4_digit(ip, 1), ipv4_digit(ip, 2), ipv4_digit(ip, 3));
+    //     }
+
+    //     while (true) {
+    //         mg_mgr_poll(&mgr, 10);
+    //     }
+
+    //     cyw43_arch_deinit();
+    vTaskDelete(NULL);
+}
+
+void print_task(__unused void *params) {
+    uint64_t count = 0;
+    uint16_t delay = 500;
+    const char *const me = pcTaskGetName(NULL);
 
     while (true) {
-        mg_mgr_poll(&mgr, 10);
-    }
+        vTaskDelay(pdMS_TO_TICKS(rand() % delay));
+        switch (rand() % 5) {
+            case 0:
+                logme(Level::ERROR, me, "UGH: %llu", count);
+                break;
+            case 1:
+                logme(Level::WARNING, me, "WOOAH: %llu", count);
+                break;
+            case 2:
+                logme(Level::INFO, me, "Wait, what?! 💀 : %llu", count);
+                logme(Level::FATAL, me, "oops ;) : %llu", count);
+                break;
+            case 3:
+                logme(Level::INFO, me, "Look!!!: %llu", count);
+                break;
+            default:
+                if (delay && !(count % (505 - delay)))
+                    delay -= 50;
+                logme(Level::TRACE, me, "NADA Here: %llu\tYesssir", count);
+                break;
+        }
+        // printf("MASK: %ul\n", ctrl.mask_dio);
+        count++;
+        if (stdio_usb_connected()) {
+            static char buf[6];
+            int c, i = 0;
+            while (((c = getchar_timeout_us(0)) != PICO_ERROR_TIMEOUT) && i < 6) {
+                buf[i] = c;
+                i++;
+                if (!strncmp(buf, "DEBUG", 5)) {
+                    logme(Level::WARNING, me, "SWITCHING TO BOOTLOADER");
+                    stdio_flush();
 
-    cyw43_arch_deinit();
+                    watchdog_hw->scratch[0] = 1;
+                    watchdog_reboot(0, 0, 1);
+                    while (1) {
+                    }
+                }
+            }
+        }
+    }
+}
+
+void vLaunch(void) {
+    TaskHandle_t task;
+    xTaskCreate(main_task, "TestMainThread", TEST_TASK_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
+    xTaskCreate(print_task, "PrintThread0", TEST_TASK_STACK_SIZE / 2, NULL, tskIDLE_PRIORITY, &task);
+    xTaskCreate(print_task, "PrintThread1", TEST_TASK_STACK_SIZE / 2, NULL, tskIDLE_PRIORITY, &task);
+    vTaskStartScheduler();
 }
 
 constexpr GPIB::PinOut gpib_pinout{
@@ -126,66 +203,35 @@ constexpr GPIB::PinOut gpib_pinout{
 GPIB::Controller ctrl(gpib_pinout, GPIB::default_config);
 // constexpr GPIB::Controller ctrl();
 
-// const Flash::SPI::Device bulk_flash{"Bulk Flash", spi0, 133 * 1000 * 1000};
+static constinit SPI::Device spi_flash_device{
+    "Main Flash",
+    SPI::Instance::SPI_0,
+    133 * 1000 * 1000,
+    {
+        .Tx = 19,
+        .Rx = 16,
+        .HOLD = 20,
+        .WP = 21,
+        .SCK = 18,
+        .CS = 17,
+    },
+};
+static SPI spi_flash_interface(spi_flash_device);
+static Flash::SPI main_flash(&spi_flash_interface);
 
-void print_task(__unused void *params) {
-    uint64_t count = 0;
-    uint16_t delay = 500;
+int main() {
+    timer_hw->dbgpause = 0;
+    bool status = stdio_init_all();
 
-    while (true) {
-        vTaskDelay(pdMS_TO_TICKS(rand() % delay));
-        switch (rand() % 5) {
-            case 0:
-                printf("[ERROR] UGH: %llu\n", count);
-                break;
-            case 1:
-                printf("[WARN] WOOAH: %llu\n", count);
-                break;
-            case 2:
-                printf("Wait, what?! 💀 : %llu\n", count);
-                printf("[FATAL] oops ;) : %llu\n", count);
-                break;
-            case 3:
-                printf("[INFO] Look!!!: %llu\n", count);
-                break;
-            default:
-                if (delay && !(count % (505 - delay)))
-                    delay -= 50;
-                printf("NADA Here: %llu\nYesssir\n", count);
-                break;
-        }
-        // printf("MASK: %ul\n", ctrl.mask_dio);
-        count++;
-        if (stdio_usb_connected()) {
-            static char buf[6];
-            int c, i = 0;
-            while (((c = getchar_timeout_us(0)) != PICO_ERROR_TIMEOUT) && i < 6) {
-                buf[i] = c;
-                i++;
-                if (!strncmp(buf, "DEBUG", 5)) {
-                    printf("SWITCHING TO BOOTLOADER\n");
-                    stdio_flush();
+    logit(Level::NOTICE, "PMPi Booting!");
 
-                    watchdog_hw->scratch[0] = 1;
-                    watchdog_reboot(0, 0, 1);
-                    while (1) {
-                    }
-                }
-            }
-        }
-    }
-}
+    // spi_flash_interface.init();
+    // main_flash.test();
 
-void vLaunch(void) {
-    TaskHandle_t task;
-    xTaskCreate(main_task, "TestMainThread", TEST_TASK_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
-    xTaskCreate(print_task, "PrintThread", TEST_TASK_STACK_SIZE / 2, NULL, tskIDLE_PRIORITY, &task);
-    vTaskStartScheduler();
-}
-
-int main(void) {
-    stdio_init_all();
     vLaunch();
+
+    while (1) {
+    }
 
     return 0;
 }
